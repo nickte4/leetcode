4fae656a513eded23a18ada3b27fee21ea73cda7
Nick Topacio <topacio.3@osu.edu>
2023-10-10 16:44:52 -0400

    Complete 190. Reverse Bits

---
+++ bits/190_reverseBits/first.js
@@ -0,0 +1,11 @@
+function reverseBits(n) {
+  let result = 0;
+  for (let i = 0; i < 32; i++) {
+    result = (result << 1) | (n & 1);
+    n >>>= 1;
+  }
+  return result >>> 0;
+}
+
+let binaryTest1 = 0b00000010100101000001111010011100;
+reverseBits(console.log(binaryTest1));
